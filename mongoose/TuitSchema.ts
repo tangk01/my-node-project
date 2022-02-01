be0b02de15dc7000e70f473d83b9e82bc92628b0
tangk01 <tang.kevi@northeastern.edu>
2022-02-01 14:34:43 -0500

    Part 3

--- mongoose/TuitSchema.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, {Schema} from "mongoose";
-import Tuit from "../models/Tuit";
-const TuitSchema = new mongoose.Schema<Tuit>({
-  tuit: {type: String, required: true},
-  postedOn: {type: Date, default: Date.now},
-  postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"}
-}, {collection: "tuits"});
-export default TuitSchema;
