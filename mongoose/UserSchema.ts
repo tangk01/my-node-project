e28e9ad7a3fefd919698a49e5fe6f731a8853482
tangk01 <tang.kevi@northeastern.edu>
2022-02-02 18:28:09 -0500

    MongoDB

--- mongoose/UserSchema.ts
+++ mongoose/UserSchema.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
-import Location from "../models/Location";
-const UserSchema = new mongoose.Schema({
+import User from "../models/User";
+const UserSchema = new mongoose.Schema<User>({
   username: {type: String, required: true},
   password: {type: String, required: true},
   firstName: String,
   lastName: String,
-  email: String,
+  email: {type: String, required: true},
   profilePhoto: String,
   headerImage: String,
-  accountType: {type: String, default: 'PERSONAL', enum: ['PERSONAL', 'ACADEMIC', 'PROFESSIONAL']},
-  maritalStatus: {type: String, default: 'SINGLE', enum: ['MARRIED', 'SINGLE', 'WIDOWED']},
   biography: String,
   dateOfBirth: Date,
-  joined: {type: Date, default: Date.now},
+  accountType: {type: String, enum: ["PERSONAL", "ACADEMIC", "PROFESSIONAL"]},
+  maritalStatus: {type: String, enum: ["MARRIED", "SINGLE", "WIDOWED"]},
   location: {
     latitude: Number,
     longitude: Number
   },
-}, {collection: 'users'});
+}, {collection: "users"});
+
 export default UserSchema;
