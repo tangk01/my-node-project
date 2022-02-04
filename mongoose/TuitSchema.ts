ea8e81f9b8970b07f4d07b1bbf90d29fa2a0317d
tangk01 <tang.kevi@northeastern.edu>
2022-02-04 13:10:03 -0500

    MongoDB Working

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -1,6 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import Tuit from "../models/Tuit";
-const TuitSchema = new mongoose.Schema<Tuit>({
+const TuitSchema = new mongoose.Schema({
   tuit: {type: String, required: true},
   postedOn: {type: Date, default: Date.now},
   postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"}
