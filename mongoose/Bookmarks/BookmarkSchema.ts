b4f45879ae57de5086459cae73ed6b0cf3b2aa48
tangk01 <tang.kevi@northeastern.edu>
2022-02-15 01:30:19 -0500

    Bookmark API

--- mongoose/Bookmarks/BookmarkSchema.ts
+++ mongoose/Bookmarks/BookmarkSchema.ts
@@ -2,7 +2,7 @@ import mongoose, {Schema} from "mongoose";
 import Bookmark from "../../models/Bookmark";
 
 const BookmarkSchema = new mongoose.Schema<Bookmark>({
-  bookmarkedTuit: {type: Schema.Types.ObjectId, ref: "UserModel"},
-  bookmarkedUser: {type: Schema.Types.ObjectId, ref: "TuitModel"},
+  bookmarkedTuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
+  bookmarkedUser: {type: Schema.Types.ObjectId, ref: "UserModel"},
 }, {collection: "bookmarks"});
 export default BookmarkSchema;
